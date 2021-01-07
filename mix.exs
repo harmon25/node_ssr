07b880a0cee1e46fdd81978b9b8e10e6904896e0
Doug W <dougwright1@gmail.com>
2021-01-07 16:00:22 -0500

    add hackney to extra apps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule NodeSsr.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :hackney],
       mod: {NodeSsr.Application, []},
       env: [
         ports: [8080],
